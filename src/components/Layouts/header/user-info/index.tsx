004aea480d1bdd7406337be99d95b01d1bafa531
Sarobidy Razafy <sarobidyraza101@gmail.com>
2025-09-17 04:56:29 +0300

    Remove unuse directories

--- src/components/Layouts/header/user-info/index.tsx
+++ src/components/Layouts/header/user-info/index.tsx
@@ -78,7 +78,7 @@ export function UserInfo() {
         <hr className="border-[#E8E8E8] dark:border-dark-3" />
 
         <div className="p-2 text-base text-[#4B5563] dark:text-dark-6 [&>*]:cursor-pointer">
-          <Link
+          {/* <Link
             href={"/profile"}
             onClick={() => setIsOpen(false)}
             className="flex w-full items-center gap-2.5 rounded-lg px-2.5 py-[9px] hover:bg-gray-2 hover:text-dark dark:hover:bg-dark-3 dark:hover:text-white"
@@ -86,7 +86,7 @@ export function UserInfo() {
             <UserIcon />
 
             <span className="mr-auto text-base font-medium">View profile</span>
-          </Link>
+          </Link> */}
 
           <Link
             href={"/pages/settings"}
--- src/components/Layouts/sidebar/data/index.ts
+++ src/components/Layouts/sidebar/data/index.ts
@@ -15,18 +15,6 @@ export const NAV_DATA = [
         ],
       },
       {
-        title: "Calendar",
-        url: "/calendar",
-        icon: Icons.Calendar,
-        items: [],
-      },
-      {
-        title: "Profile",
-        url: "/profile",
-        icon: Icons.User,
-        items: [],
-      },
-      {
         title: "User role",
         icon: Icons.User,
         items: [
